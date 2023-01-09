6bf1142086eab94c733bbc6a68387880d3332182
MikeSanDev <mjs070594@gmail.com>
2023-01-08 22:14:57 -0600

    responsive

--- chamber_io/src/productsStore.js
+++ chamber_io/src/productsStore.js
@@ -1,28 +1,28 @@
 // GA Ticket: price_1MF6w3A1869aNZSewqBrWimT
 // Midnight: price_1MF6xWA1869aNZSe8Iddql1g
-
+// Day 1: price_1MOCQJA1869aNZSe1mR8RuUl
 // if creating new product, use the API product keys above the id
 const productsArray = [
+    {
+        id: "price_1MOCQJA1869aNZSe1mR8RuUl",
+        title: "Day 1 Admission ",
+        description: "Day 1 session - Chamber v7",
+        image: "/images/hyperbolic-time-chamber-dbz.jpeg",
+        price: 100.00
+    },
+    {
+        id: "price_1MF6xWA1869aNZSe8Iddql1g",
+        title: "'Midnight' [Chamber]",
+        description: "Day 2 midnight - Chamber v7",
+        image: "/images/midnight.png",
+        price: 50.00
+    },
     {
         id: "price_1MF6w3A1869aNZSewqBrWimT",
         title: "General Admission ",
         description: "Full 2 day pass to Chamber v7",
         image: "/images/front.png",
         price: 150.00
-    },
-    {
-        id: "price_1MF6xWA1869aNZSe8Iddql1g",
-        title: "'Midnight' [Chamber]",
-        description: "Day 2 midnight session - Chamber v7",
-        image: "/images/midnight.png",
-        price: 50.00
-    },
-    {
-        id: "3",
-        title: " example product ",
-        description: " session - Chamber v7",
-        image: "/images/hyperbolic-time-chamber-dbz.jpeg",
-        price: 50.00
     }
 ]
 
